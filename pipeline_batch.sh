80c9d96f04963b7b6a8862158948b6433c6b9d3c
hbhutta <haadbhutta@gmail.com>
2025-07-15 20:50:05 +0000

    Added check for Hb correction value, exiting if it's not found

--- pipeline_batch.sh
+++ pipeline_batch.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-:'
-Author: Haad Bhutta
-Updated: July 15, 2025
-
-Description:
-This script runs the separate files that make up the pipeline. The order of the 
-files that are run cannot be changed. 
-
-The input to this pipeline is the path toa cohort directory containing individual patient directories.
-
-Example usage:
-bash pipeline_batch.sh cohort/
-'
-
 set -e # This will make sure that this bash script will immediately end if any error occurs
 
 cohort=$1 # Source directory containing patient directories
 rbc_m_ratios_data=csvs/cohort_rbc_mem_ratios.csv
+hb_correction_data=csvs/cohort_hb_corrections.csv
+
+# Define log file
+timestamp=$(date +"%Y_%m_%d_%H%M%S")
+log_file="runs/${timestamp}_batch.txt"
+
+echo "Received cohort directory: $cohort"
+echo "Writing output to log file: $log_file"
 
 for patient in "$cohort"/*; do
     if [ -d "$patient" ]; then
-        echo "Processing patient directory: $patient"
-        
-        rbc_m_ratio=$(awk -F',' -v name="$patient" '$1 == name {print $2}' "$csv_file")
+        patient_id=$(basename "$patient")
+
+        rbc_m_ratio=$(awk -F',' -v name="$patient" '$1 == name {print $2}' "$rbc_m_ratios_data") 
         if [[ -z "$rbc_m_ratio" ]]; then
-            echo "ERROR: RBC:Membrane ratio not found for patient ${patient} in ${rbc_m_ratios_data}. Exiting."
+            echo "ERROR: RBC:Membrane ratio not found for patient ${patient_id} in ${rbc_m_ratios_data}. Exiting."
             exit 1
         else
-            echo "Recieved rbc:membrane ratio of ${rbc_m_ratio} for patient ${patient}"
+            echo "Recieved rbc:membrane ratio of ${rbc_m_ratio} for patient ${patient_id}"
         fi
 
-        bash pipeline_single.sh "$patient"
+        hb_correction_value=$(awk -F',' -v name="$patient" '$1 == name {print $2}' "$hb_correction_data")
+        if [[ -z "$hb_correction_value" ]]; then
+            echo "ERROR: Hb correction value not found for patient ${patient_id} in ${hb_corrections_data}. Exiting."
+            exit 1
+        else
+            echo "Recieved Hb correction value of ${hb_correction_value} for patient ${patient_id}"
+        fi
+
+        bash pipeline_single.sh "$patient" > "$log_file" 2>&1
     else
         echo "Skipping non-directory: $patient"
     fi
