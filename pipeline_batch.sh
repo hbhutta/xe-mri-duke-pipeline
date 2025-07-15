5bba4b1cf2fa9176220fa412bbf412b99190f642
hbhutta <haadbhutta@gmail.com>
2025-07-15 18:50:05 +0000

    Added condition to skip a patient in batch processing if their RBC:M ratio is not found in the CSV file in csvs/

--- pipeline_batch.sh
+++ pipeline_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-: '
+:'
 Author: Haad Bhutta
-Updated: June 30, 2025
+Updated: July 15, 2025
 
 Description:
 This script runs the separate files that make up the pipeline. The order of the 
@@ -16,11 +16,21 @@ bash pipeline_batch.sh cohort/
 
 set -e # This will make sure that this bash script will immediately end if any error occurs
 
-cohort=$1
+cohort=$1 # Source directory containing patient directories
+rbc_m_ratios_data=csvs/cohort_rbc_mem_ratios.csv
 
 for patient in "$cohort"/*; do
     if [ -d "$patient" ]; then
         echo "Processing patient directory: $patient"
+        
+        rbc_m_ratio=$(awk -F',' -v name="$patient" '$1 == name {print $2}' "$csv_file")
+        if [[ -z "$rbc_m_ratio" ]]; then
+            echo "ERROR: RBC:Membrane ratio not found for patient ${patient} in ${rbc_m_ratios_data}. Exiting."
+            exit 1
+        else
+            echo "Recieved rbc:membrane ratio of ${rbc_m_ratio} for patient ${patient}"
+        fi
+
         bash pipeline_single.sh "$patient"
     else
         echo "Skipping non-directory: $patient"
